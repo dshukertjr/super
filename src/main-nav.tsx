3957bb29fde7e847ac00e6588f2b887ab45e6c40
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2020-11-05 12:53:54 +0900

    minor adjustment

--- src/main-nav.tsx
+++ src/main-nav.tsx
@@ -15,6 +15,25 @@ import List from '@material-ui/core/List';
 import clsx from 'clsx';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Badge from '@material-ui/core/Badge';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+        </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
 
 const drawerWidth = 240;
 
@@ -111,6 +130,9 @@ function MainNav() {
         setOpen(false);
     };
 
+    const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+
     return (
         <div className={classes.root}>
             <CssBaseline />
@@ -152,7 +174,31 @@ function MainNav() {
                 <Divider />
                 <List>{secondaryMenuItems}</List>
             </Drawer>
-            <main className={classes.content}><div className={classes.appBarSpacer} />this is the main</main>
+            <main className={classes.content}>
+                <div className={classes.appBarSpacer} />
+                <Container maxWidth="lg" className={classes.container}>
+                    <Grid container spacing={3}>
+                        {/* Chart */}
+                        <Grid item xs={12} md={8} lg={9}>
+                            <Paper className={fixedHeightPaper}>
+                            </Paper>
+                        </Grid>
+                        {/* Recent Deposits */}
+                        <Grid item xs={12} md={4} lg={3}>
+                            <Paper className={fixedHeightPaper}>
+                            </Paper>
+                        </Grid>
+                        {/* Recent Orders */}
+                        <Grid item xs={12}>
+                            <Paper className={classes.paper}>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                    <Box pt={4}>
+                        <Copyright />
+                    </Box>
+                </Container>
+            </main>
         </div>
     );
 }
